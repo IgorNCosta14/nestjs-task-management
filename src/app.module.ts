9a3cd260520e741f9e596ce7bbf0c21ea84f599e
Igor Nascimento Costa <igorncosta14@hotmail.com>
2023-01-13 18:37:38 -0300

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+    }),
+  ],
 })
 export class AppModule {}
